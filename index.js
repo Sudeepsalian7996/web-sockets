bfd2aa18f6487eb459f66f4e260d07ea083ab1d5
sudeep7996 <sudeep7996@gmail.com>
2024-06-22 13:09:54 +0530

    new line

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const socket=new WebSocket("ws://localhost:3000")
+const socket = new WebSocket("ws://localhost:3000");
 // socket.onopen=function(event){
 //     alert("socket is connected successfully")
 //     socket.send("hey")
 //     console.log(event)
 // }
-const message=document.getElementById("logs")
-socket.onmessage=function (event){
-    console.log(event)
-    message.innerHTML+=event.data+"<br>"
-}
-document.getElementById("send").onclick=function(){
-    const text=document.getElementById("msg").value
-    message.innerHTML+="You:-"+text+"<br>"
-    socket.send(text)
-}
+const message = document.getElementById("logs");
+socket.onmessage = function (event) {
+  console.log(event);
+  message.innerHTML += event.data + "<br>";
+};
+document.getElementById("send").onclick = function () {
+  const text = document.getElementById("msg").value;
+  message.innerHTML += "You:-" + text + "<br>";
+  socket.send(text);
+};
+
+console.log("new line added");
